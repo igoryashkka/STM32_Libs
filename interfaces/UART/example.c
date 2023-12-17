54c2098044ef20bc27a000bee1119c39803ea4c9
igoryashkka <igor404ik@gmail.com>
2023-12-17 17:49:58 +0200

    UART example folder

---
+++ interfaces/UART/example.c
@@ -0,0 +1,2 @@
+// write the program that will be run on some OS like linux | MACos | windows and get the info from COM port 
+// to the COM port will be connected stm32 with Transmitting data from stm32x
